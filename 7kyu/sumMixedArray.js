17d7359f8f7c1ca5177a07f1e35e5a2256077f80
Christopher Munoz <102195481+ChrisMunozCodes@users.noreply.github.com>
2022-12-15 12:26:44 -0500

    Delete sumMixedArray.js

--- 7kyu/sumMixedArray.js
+++
@@ -1,8 +0,0 @@
-function sumMix(x){
-    let numberArray = x.map(Number)
-    
-    const sum = numberArray.reduce((accumulator, value) => {
-    return accumulator + value;
-    }, 0);
-    return sum
-  }
