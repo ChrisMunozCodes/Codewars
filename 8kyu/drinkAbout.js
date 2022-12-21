8026ee10d3554b94482b7abd823bd52741fd55d1
Christopher Munoz <christopherm0507@gmail.com>
2022-12-21 12:09:45 -0500

    added file drinkAbout.js

---
+++ 8kyu/drinkAbout.js
@@ -0,0 +1,14 @@
+function peopleWithAgeDrink(old) {
+    if (old < 14) {
+      return 'drink toddy'
+    }
+    if (old <= 17) {
+      return 'drink coke'
+    }
+    if (old <= 20) {
+      return 'drink beer'
+    }
+    if (old <= 30) {
+      return 'drink whisky'
+    }
+};
