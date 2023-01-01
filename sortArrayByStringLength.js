5b7adb8cdc5a4df85399fe4034348d23d2d3cb08
Christopher Munoz <102195481+ChrisMunozCodes@users.noreply.github.com>
2022-12-31 19:09:03 -0500

    Delete sortArrayByStringLength.js

--- sortArrayByStringLength.js
+++
@@ -1,7 +0,0 @@
-function sortByLength (array) {
-  // Return an array containing the same strings, ordered from shortest to longest
-  for (let i = 0; i < array.length; i++)   {
-    let sortedArr = array.sort((a,b) => a.length - b.length)
-    return sortedArr
-  }
-};
