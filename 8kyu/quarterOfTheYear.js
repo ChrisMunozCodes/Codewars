d86c17830415020e0db8667df5f0ecf7e8641088
Christopher Munoz <christopherm0507@gmail.com>
2023-01-09 09:34:51 -0500

    added file quarterOfTheYear.js

---
+++ 8kyu/quarterOfTheYear.js
@@ -0,0 +1,44 @@
+const quarterOf = (month) => {
+    switch(month) {
+     case 0: 
+     return 1
+     break;
+     case 1: 
+     return 1
+     break;
+     case 2: 
+     return 1
+     break;
+     case 3: 
+     return 1
+     break;
+     case 4: 
+     return 2
+     break;
+     case 5: 
+     return 2
+     break;
+     case 6: 
+     return 2
+     break;
+     case 7: 
+     return 3
+     break;
+     case 8: 
+     return 3
+     break;
+     case 9: 
+     return 3
+     break;
+     case 10: 
+     return 4
+     break;
+     case 11: 
+     return 4
+     break;
+     case 12: 
+     return 4
+     break;
+    }
+    
+}
