e4fc437d8253f996da67f07b2826f672f98f6bea
Christopher Munoz <christopherm0507@gmail.com>
2023-01-11 13:34:01 -0500

    added file makeAFunctionThatDoesArithmetic!.js

---
+++ 7kyu/makeAFunctionThatDoesArithmetic!.js
@@ -0,0 +1,11 @@
+function arithmetic(a, b, operator){
+    if (operator === 'add') {
+      return a + b
+    } else if (operator === 'subtract') {
+      return a - b
+    } else if (operator === 'multiply') {
+      return a * b
+    } else if (operator === 'divide') {
+      return a / b
+    }
+}
