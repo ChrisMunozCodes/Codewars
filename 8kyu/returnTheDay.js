9b8e44d67a89032c2c791080e3e015945cdcd437
Christopher Munoz <christopherm0507@gmail.com>
2023-02-10 17:52:18 -0500

    added file returnTheDay.js

---
+++ 8kyu/returnTheDay.js
@@ -0,0 +1,26 @@
+function whatday(num) { 
+    switch(num){
+      case 1:
+        return "Sunday"
+        break;
+      case 2:
+        return "Monday"
+        break;
+      case 3:
+        return "Tuesday"
+        break;
+      case 4:
+        return "Wednesday"
+        break;
+      case 5:
+        return "Thursday"
+        break;
+      case 6:
+        return "Friday"
+        break;
+      case 7:
+        return "Saturday"
+        break;
+    }
+    return "Wrong, please enter a number between 1 and 7"
+}
