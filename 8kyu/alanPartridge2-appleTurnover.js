8c5234fc2e2e15b205165d43461ca9fc46f89717
Christopher Munoz <christopherm0507@gmail.com>
2023-03-02 19:34:30 -0500

    added file alanPartridge2-appleTurnover.js

---
+++ 8kyu/alanPartridge2-appleTurnover.js
@@ -0,0 +1,9 @@
+//Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
+
+function apple(x){
+    if(Math.pow(x, 2) > 1000){
+      return "It's hotter than the sun!!"
+    } else {
+      return "Help yourself to a honeycomb Yorkie for the glovebox."
+    }
+}
