c55f684750c2faebaf5b5de05b47676fb9b7f3a3
Christopher Munoz <christopherm0507@gmail.com>
2023-04-09 22:27:34 -0400

    Added file dna-to-rna-conversion.js

--- 8kyu/dna-to-rna-conversion.js
+++ 8kyu/dna-to-rna-conversion.js
@@ -18,14 +18,14 @@ function DNAtoRNA(dna) {
     const splitedDna = dna.split('')
     let finalArr = []
   
-    for(i = 0; i < splitedDna.length; i++) {
+    for(i = 0; i < splitedDna.length; i++){
         if(splitedDna[i] === 'T') {
           finalArr.push('U')
         } else {
           finalArr.push(splitedDna[i])
         }
     }
-    
+
      return finalArr.join('')
 
 }
