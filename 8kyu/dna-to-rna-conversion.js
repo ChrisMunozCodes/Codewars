571d8baa6d21ae3f7852e1e0defbbaf4dbbb623d
Christopher Munoz <christopherm0507@gmail.com>
2023-04-24 22:24:55 -0400

    Added file dna-to-rna-conversion.js

--- 8kyu/dna-to-rna-conversion.js
+++ 8kyu/dna-to-rna-conversion.js
@@ -6,8 +6,8 @@
 //RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
 
 // Create a function which translates a given DNA string into RNA.
-// For example:
 
+// For example:
 // "GCAT"  =>  "GCAU"
 
 // The input string can be of arbitrary length - in particular, it may be empty. 
