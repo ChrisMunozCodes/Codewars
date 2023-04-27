d815eba4d32e0c39770a42051debbaf7fda0c7d6
Christopher Munoz <christopherm0507@gmail.com>
2023-04-26 22:24:13 -0400

    Added file dna-to-rna-conversion.js

--- 8kyu/dna-to-rna-conversion.js
+++ 8kyu/dna-to-rna-conversion.js
@@ -2,9 +2,7 @@
 //It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
 
 // Ribonucleic acid, RNA, is the primary messenger molecule in cells. 
-
 //RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
-
 
 // Create a function which translates a given DNA string into RNA.
 // For example:
