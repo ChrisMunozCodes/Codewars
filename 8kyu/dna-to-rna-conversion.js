e90ec6fc175c1c067f96e7313ab089f935d54fb0
Christopher Munoz <christopherm0507@gmail.com>
2023-05-05 22:21:07 -0400

    Added file dna-to-rna-conversion.js

--- 8kyu/dna-to-rna-conversion.js
+++ 8kyu/dna-to-rna-conversion.js
@@ -5,9 +5,9 @@
 // Create a function which translates a given DNA string into RNA.
 
 // For example:
-
 // "GCAT"  =>  "GCAU"
-// The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+// The input string can be of arbitrary length - in particular, it may be empty. 
+//All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
 
 function DNAtoRNA(dna) {
     const splitedDna = dna.split('')
