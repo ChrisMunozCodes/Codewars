ec0a846c371f405c71c9c93493df8750ae4cd204
Christopher Munoz <christopherm0507@gmail.com>
2023-06-01 22:41:23 -0400

    Added 8kyu codewar Calculate Average

--- 8kyu/calculateAverage.js
+++ 8kyu/calculateAverage.js
@@ -1,10 +1,8 @@
 function findAverage(array) {
-
     // empty array returns 0
     if(array.length === 0) {
       return 0;
     }
   
     return array.reduce((a,b) => a + b, 0) / array.length
-    
 }
