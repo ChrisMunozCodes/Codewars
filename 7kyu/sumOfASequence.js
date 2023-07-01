8deee888d5d8b09085778e8fbbae8d352bef7cd9
Christopher Munoz <christopherm0507@gmail.com>
2023-06-30 22:31:43 -0400

    Added 7kyu codewar solution for Sum of a Sequence

---
+++ 7kyu/sumOfASequence.js
@@ -0,0 +1,15 @@
+const sequenceSum = (begin, end, step) => {
+    //If begin value is greater than the end, your function should return 0
+    if(begin > end) {
+      return 0
+    } 
+  
+    let arrList = []
+    
+    //
+    for(let i = begin; i <= end; i+=step) {
+        arrList.push(i)
+    }
+    
+    return arrList.reduce((a,b) => a+b)
+  };
