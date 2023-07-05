98a2669f70635a6be490d5775eff0b707215f72f
Christopher Munoz <christopherm0507@gmail.com>
2023-07-04 22:28:01 -0400

    Added 8kyu codewar solution for Is it a palindrome?

--- 8kyu/isItAPalindrome.js
+++ 8kyu/isItAPalindrome.js
@@ -2,6 +2,7 @@ function isPalindrome(x) {
     let intoArr = x.split('')
     const reversedArr = intoArr.reverse((a,b) => a - b).join('').toLowerCase()
     
+    
     if(x.toLowerCase() === reversedArr){
       return true
     } else {
