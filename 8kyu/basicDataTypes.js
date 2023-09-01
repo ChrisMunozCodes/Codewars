1602b904ae0269b32e9a139fcf5adf2f95f0bddc
Christopher Munoz <christopherm0507@gmail.com>
2023-09-01 11:55:56 -0400

    8kyu codewar solution for a basic data types

---
+++ 8kyu/basicDataTypes.js
@@ -0,0 +1,37 @@
+let v1 = 50,
+    v2 = 100,
+    v3 = 150,
+    v4 = 200,
+    v5 = 2,
+    v6 = 250;
+
+function equal1(){
+  let a = v1,   
+      b = v1;   
+  return a + b;
+}
+
+//Please refer to the example above to complete the following functions
+function equal2(){
+  let a =  v3,
+      b =  v1
+  return a - b;
+}
+
+function equal3(){
+  let a =  v5,
+      b =  v1
+  return a * b;
+}
+
+function equal4(){
+  let a =  v4,
+      b =  v5
+  return a / b;
+}
+
+function equal5(){
+  let a =  v2,
+      b =  v4
+  return a % b;
+}
